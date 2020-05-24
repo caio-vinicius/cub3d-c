7b92e983d77bb6a20446f1d306b1c4151bf62a86
caio-vinicius <sf.caio92@gmail.com>
2020-05-23 21:15:45 -0300

    new structure

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: caio <csouza-f@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/23 13:10:29 by caio              #+#    #+#             */
-/*   Updated: 2020/05/23 13:14:59 by caio             ###   ########.fr       */
+/*   Updated: 2020/05/23 20:54:28 by caio             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,30 @@
 
 # include <mlx.h>
 # include <math.h>
+// remeber to remove
 # include <stdio.h>
+
+# define M_KEYPRESS 1L<<0
+# define M_KEYRELEASE 1L<<1
+# define E_KEYPRESS 2
+# define E_KEYRELEASE 3
+
+# define WINDOW_WIDTH 600
+# define WINDOW_HEIGHT 400
 
 typedef struct	s_vars {
 	void	*init;
 	void	*window;
 }		t_vars;
+
+typedef struct	s_player {
+	int	x;
+	int	y;
+}		t_player;
+
+typedef struct		s_all {
+	t_vars		*vars;
+	t_player	*player;
+}			t_all;
 
 #endif
--- main.c
+++ main.c
@@ -6,16 +6,68 @@
 /*   By: caio <csouza-f@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/23 13:10:00 by caio              #+#    #+#             */
-/*   Updated: 2020/05/23 13:16:52 by caio             ###   ########.fr       */
+/*   Updated: 2020/05/23 21:10:22 by caio             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+#include <unistd.h>
 
-int main(void)
+t_vars	*init(t_vars *vars)
 {
-	t_vars vars;
-	vars.init = mlx_init();
-	vars.window = mlx_new_window(vars.init, 200, 200, "cub3d");
-	mlx_loop(vars.init);
+	vars->init = mlx_init();
+	vars->window = mlx_new_window(vars->init, WINDOW_WIDTH, WINDOW_HEIGHT, "cub3d");
+	if (vars->init && vars->window)
+		return (vars);
+	return (NULL);
+}
+
+void	render()
+{
+	
+}
+
+void	update(t_player *player)
+{
+	player->x += 1;
+	player->y += 1;
+}
+
+void	setup(t_player *player)
+{
+	player->x = 0;
+	player->y = 0;
+}
+
+void	process_keys(int keycode, t_vars *vars)
+{
+	if (keycode == 65307)
+		mlx_destroy_window(vars->init, vars->window);
+	printf("%d\n", keycode);
+}
+
+int	game_loop(int keycode, t_all *all)
+{
+	//sleep(1);
+	process_keys(keycode, all->vars);
+	update(all->player);
+	render();
+}
+
+int	main(void)
+{
+	t_all *all;
+	t_vars *vars; 
+
+	vars = init(vars);
+	all->vars = vars;
+
+	setup(all->player);
+
+	if (all->vars)
+	{
+		mlx_hook(all->vars->window, E_KEYPRESS, M_KEYPRESS, game_loop, &all);
+		//mlx_loop_hook(vars->init, game_loop, &all);
+		mlx_loop(all->vars->init);
+	}
 }
